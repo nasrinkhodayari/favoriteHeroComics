d407adfb8f0420c77e1f03cc5abf38ce246a552d
nirsaan <nasrin.khodayari@gmail.com>
2020-03-12 14:44:17 +0330

    Add ItemsList component first init

---
+++ src/components/ItemsList/index.tsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme: Theme) => createStyles({}));
+const ItemsList = () => {
+  useEffect(() => {}, []);
+
+  return <div>Hi :)</div>;
+};
+
+export default ItemsList;
